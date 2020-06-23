8948ef0f2fb53868be48a4c79868e3e8c4bc774a
LVSMix <lvsmix@gmail.com>
2020-06-23 00:48:37 -0300

    le agrego handlebars

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Site de BANodejs by Luis Ventocilla
 
 Only backend api
+
+view-engine ==> npm install express-handlebars
--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 var express = require("express"),
-    service = require("./services/service.js");
+    service = require("./services/service.js"),
+    exphbs  = require('express-handlebars');
 
 var app = express();
 
-app.get("/",function(req,res){
+// Set Path for views
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+app.get("/api",function(req,res){
     res.send(service.saludo());
+})
+
+app.get("/",function(req,res){
+    res.render('home');
 });
 
 app.listen(3030);
