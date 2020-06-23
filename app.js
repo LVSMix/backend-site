9716de4cfbdee532ddc4b8f986590bba4c50dc70
LVSMix <lvsmix@gmail.com>
2020-06-23 01:22:36 -0300

    se agrega middleware

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 var express = require("express"),
     service = require("./services/service.js"),
-    exphbs  = require('express-handlebars');
+    exphbs  = require('express-handlebars'),
+    bodyParser = require('body-parser');
 
 var app = express();
+
+//middleware
+app.use("public",express.static('public'));
+
+//body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 
 // Set Path for views
 app.engine('handlebars', exphbs());
@@ -15,6 +23,10 @@ app.get("/api",function(req,res){
 app.get("/",function(req,res){
     res.render('home');
 });
+
+app.post("/contacto",function(req,res){
+    res.send("Envio Completado");
+})
 
 app.listen(3030);
 
